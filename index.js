5652b6f14b634f14513f6e2ddaa6815bdd6404a8
soruly <soruly@gmail.com>
2021-01-27 17:32:21 +0000

    Convert to ESModule

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-require("dotenv").config();
-
-const readline = require("readline");
-const fs = require("fs-extra");
-const fetch = require("node-fetch");
-const OpenCC = require("opencc");
-const opencc = new OpenCC("s2t.json");
-const { PlurkClient } = require("plurk2");
+import "dotenv/config.js";
+import readline from "readline";
+import fs from "fs-extra";
+import fetch from "node-fetch";
+import OpenCC from "opencc";
+import { PlurkClient } from "plurk2";
 
 const { CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET } = process.env;
 
@@ -14,40 +12,38 @@ if (!CONSUMER_KEY || !CONSUMER_SECRET) {
   process.exit();
 }
 
-(async () => {
-  if (!TOKEN || !TOKEN_SECRET) {
-    const client = new PlurkClient(CONSUMER_KEY, CONSUMER_SECRET);
-    const { authPage } = await client.getRequestToken();
-    console.log(`Please authorize this app in ${authPage}`);
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    const code = await new Promise((resolve) => rl.question("Enter verification code: ", resolve));
-    rl.close();
-    await client.getAccessToken(code);
-    console.log("Saving token in .env...");
-    fs.writeFileSync(
-      ".env",
-      [
-        fs.readFileSync(".env", "utf8"),
-        `TOKEN=${client.token}`,
-        `TOKEN_SECRET=${client.tokenSecret}`,
-      ].join("\n")
-    );
-    const me = await client.request("Users/me");
-    console.log(`Welcome ${me.display_name}`);
-    process.exit();
-  }
-  const client = new PlurkClient(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET);
-  const hitokoto = await fetch("https://v1.hitokoto.cn").then((res) => res.json());
-  const plurk = await client.request("Timeline/plurkAdd", {
-    content: opencc.convertSync(`${hitokoto.hitokoto} [emo76]\n -- ${hitokoto.from}`),
-    qualifier: ":",
+if (!TOKEN || !TOKEN_SECRET) {
+  const client = new PlurkClient(CONSUMER_KEY, CONSUMER_SECRET);
+  const { authPage } = await client.getRequestToken();
+  console.log(`Please authorize this app in ${authPage}`);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
-  await client.request("Responses/responseAdd", {
-    plurk_id: plurk.plurk_id,
-    content: `https://hitokoto.cn/?id=${hitokoto.id}`,
-    qualifier: ":",
-  });
-})();
+  const code = await new Promise((resolve) => rl.question("Enter verification code: ", resolve));
+  rl.close();
+  await client.getAccessToken(code);
+  console.log("Saving token in .env...");
+  fs.writeFileSync(
+    ".env",
+    [
+      fs.readFileSync(".env", "utf8"),
+      `TOKEN=${client.token}`,
+      `TOKEN_SECRET=${client.tokenSecret}`,
+    ].join("\n")
+  );
+  const me = await client.request("Users/me");
+  console.log(`Welcome ${me.display_name}`);
+  process.exit();
+}
+const client = new PlurkClient(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET);
+const hitokoto = await fetch("https://v1.hitokoto.cn").then((res) => res.json());
+const plurk = await client.request("Timeline/plurkAdd", {
+  content: new OpenCC("s2t.json").convertSync(`${hitokoto.hitokoto} [emo76]\n -- ${hitokoto.from}`),
+  qualifier: ":",
+});
+await client.request("Responses/responseAdd", {
+  plurk_id: plurk.plurk_id,
+  content: `https://hitokoto.cn/?id=${hitokoto.id}`,
+  qualifier: ":",
+});
